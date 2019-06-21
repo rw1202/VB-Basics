42ab3bbe22acd87e380edc814ab8b35ba6f15c04
rw1202 <44084938+rw1202@users.noreply.github.com>
2019-06-21 09:38:45 +0100

    Add files via upload
    Decisions

---
+++ Decision.vb
@@ -0,0 +1,67 @@
+﻿Public Class Decision
+    Public Shared Function FoeIf() As String 
+        Dim products= Lists.LoadProducts()
+        
+        For index As Integer =0 To (products.Count-1)
+            If products(index).Name= "10 Speed Bike"
+                Console.WriteLine("Name : " _
+                              & products(index).Name.ToString())
+                  Console.ReadKey()
+            End If
+         
+        Next
+        
+    End Function
+
+    Public Shared Function FoeElIf() As String
+        Dim products = Lists.LoadProducts()
+
+        For index As Integer = 0 To (products.Count-1)
+            If products(index).Name = "10 Speed Bike"
+                Console.WriteLine("Product Search (10 Speed Bike) : " _
+                                  & products(index).ToString())
+                Else 
+                    Console.WriteLine("All Others: " _
+                                      & products(Index).ToString())
+            End If
+           
+        Next
+        Console.ReadKey()
+    End Function 
+
+    Public Shared Function FoeElIfEl() As String
+        Dim products = Lists.LoadProducts()
+        Dim search100 As New List(Of Product)()
+        Dim allOthers As New List (Of Product)()
+        For index As Integer = 0 To (products.Count-1)
+            If products(Index).Name="10 Speed Bike"
+                Console.WriteLine("Product Search (10 Speed Bike): " _
+                                  & products(Index).ToString())
+
+                ElseIf products(Index).ListPrice = 100
+                search100.Add(products(Index))
+'                    
+                    Else 
+                     AllOthers.Add(products(Index))
+                 
+            End If
+           
+                            
+        Next
+
+        Console.WriteLine("Product Search (List Price 100): ")
+        For Each element In search100
+            Console.WriteLine(element.ToString())
+        Next
+
+        Console.WriteLine("All Others: " )
+        For Each element In allOthers
+            Console.WriteLine(element.ToString())
+            
+                              
+        Next
+        Console.ReadKey()
+    End Function
+   
+
+End Class
--- MainModule1.vb
+++ MainModule1.vb
@@ -13,7 +13,10 @@
         'Dim loopProds As String= Loops.DewItWhile()
         'Dim loopProds1 As String= Loops.DewIt()
 '         
-       Dim loopPods2 As String= Loops.ForE()
+'       Dim loopPods2 As String= Loops.ForE()
+'       Dim Floop As String = Decision.FoeIf()
+'       Dim Floop2 As String = Decision.FoeElIf()
+       Dim Floop3 As String = Decision.FoeElIfEl()
    End Sub
     
     End Module
